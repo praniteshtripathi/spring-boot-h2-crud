838e65f7ad965f12b7bd55573ae67ea56c9d2201
LAPTOP-8OTS97CR\pranitesh tripathi <pranitesh@gmail.com>
2022-07-17 13:00:26 +0530

    added file in git

---
+++ src/main/java/com/rnd/h2/SpringBootH2CrudApplication.java
@@ -0,0 +1,13 @@
+package com.rnd.h2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootH2CrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootH2CrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rnd/h2/SpringBootH2CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rnd.h2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootH2CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
